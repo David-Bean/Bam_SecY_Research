77a11e0a7307edeeec943c1e82818de38ae1ab7c
David-Bean <146380480+David-Bean@users.noreply.github.com>
2023-10-07 10:06:50 -0600

    Add files via upload
    Python scripts for BLAST, uses the first sequence in a fasta file

---
+++ BLAST_Scripts/blast_stuff.py
@@ -0,0 +1,53 @@
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+from Bio import SeqIO
+from Bio import SearchIO
+from Bio import AlignIO # <--Like SeqIO but for aligned files I guess
+
+
+
+'''
+Takes a fasta file as input, runs protein-protein BLAST for the first sequence in the file
+saves results as blastrsults.xml "blastq"
+
+https://biopython.org/docs/1.75/api/Bio.Blast.NCBIWWW.html
+This is basically all the documentation for blastq I've been able to find so far
+
+Should maybe a way to filter only one per species?
+'''
+def prot_blast(input_file,evalue):
+
+	input_fa = SeqIO.parse(input_file,"fasta")
+	input_seq = next(input_fa) # <- Next chooses the next item in an iterator
+	print(f'Blasting sequence: {input_seq.id}...\nBLASTING...')
+
+	result_handle = NCBIWWW.qblast(
+		program = 'blastp',
+		database = 'swissprot', # <--- Trying different databases. Next i'm trying uniprot (swissprot)
+		sequence = input_seq.seq,
+		expect = evalue,
+		short_query = False,
+		hitlist_size = 200,
+		filter = None
+		)
+	blast_xml = result_handle.read()
+
+	with open('BlastResults.xml','w') as save_file:
+		save_file.write(blast_xml)
+	print('result saved')
+
+
+
+'''
+Converts xml file to fasta
+'''
+def xml_fasta(results_name):
+	blast = SearchIO.read('BlastResults.xml','blast-xml')
+	records = []
+	for hit in blast:
+		records.append(hit[0].hit)
+	SeqIO.write(records,results_name+'.fa','fasta')
+	print(f'{len(records)} hits found!')
+
+
+
