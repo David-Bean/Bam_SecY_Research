bd2d127c41535747133870bd8e1f4481aba14668
David-Bean <146380480+David-Bean@users.noreply.github.com>
2023-10-09 13:38:02 -0600

    Delete Parsingfasta directory

--- Parsingfasta/SANDBOX/tinker.py
+++
@@ -1,5 +0,0 @@
-import fasta_parsingSANDBOX as fasta_parsing
-
-parser = fasta_parsing.Fasta_parser()
-parser.extract_names('enterobacterales.fa')
-parser.cross_ref_nrs('BamA_UniProt.fasta','enterobacterales.fa','cross_ref')
